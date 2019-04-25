513c197e927d9a66f4a4d74da13aa9b26436e987
jw397 <jw397@126.com>
2019-04-25 21:47:38 +0800

    feat: 加参数

--- index.js
+++ index.js
@@ -2,51 +2,43 @@ const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpack = require('webpack');
 
-module.exports = function(){
-	const plugins = [];
-	const opts = process.argv.slice(2);
+module.exports = function (config = {}) {
+  const plugins = [];
+  const opts = process.argv.slice(2);
 
-	let defaultConfig = {
-	  entry: {
-		'util-common': './src/common/index.js',
-		'util': './src/browser/index.js'
-	  },
-	  output: {
-		libraryTarget: 'umd', //umd
-		// umdNamedDefine: true,
-		globalObject: 'this', //必须得有它
-		path: path.resolve(__dirname, 'release'),
-		// filename: 'Q.js'
-		library: 'utils'
-	  },
-	  // externals: {
-	  //   bluebird: 'bluebird'
-	  // },
-	  plugins: plugins
-	};
+  let defaultConfig = {
+    entry: {
+      // 'util-common': './src/common/index.js',
+      // 'util': './src/browser/index.js'
+    },
+    output: {
+      libraryTarget: 'umd', //umd
+      // umdNamedDefine: true,
+      globalObject: 'this', //必须得有它
+      path: path.resolve(__dirname, 'release'),
+      // filename: 'Q.js'
+      library: 'utils'
+    },
+    // externals: {
+    //   bluebird: 'bluebird'
+    // },
+    plugins: plugins
+  };
 
-	let config = {};
-	if (!opts.includes('production')) { //开发环境
-	  config = {
-		devtool: 'inline-source-map'
-		// devServer: {
-		//   port: 8089
-		// },
-	  };
-	  [].push.apply(plugins, [
-		new webpack.NamedModulesPlugin(),
-		new webpack.HotModuleReplacementPlugin(),
-		new BundleAnalyzerPlugin()//分析bundle代码组成
-	  ]);
-	} else {
+  if (!opts.includes('production')) { //开发环境
+    defaultConfig.devtool = 'inline-source-map';
+    [].push.apply(plugins, [
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new BundleAnalyzerPlugin()//分析bundle代码组成
+    ]);
+  }
 
-	}
+  for (var key in defaultConfig) {
+    if (!config[key]) {
+      config[key] = defaultConfig[key];
+    }
+  }
 
-	for (var key in defaultConfig) {
-	  if (!config[key]) {
-		config[key] = defaultConfig[key];
-	  }
-	}
-
-	return config;	
+  return config;
 };
